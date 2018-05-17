a4c70b647adec3d9c4f8dbc1facb22102ebb81c4
Punit Panchal <punit@riverbridgeventures.com>
2018-05-17 16:48:28 +0530

    php to python v1.0.11

---
+++ index6.php
@@ -0,0 +1,85 @@
+<?php
+//error_reporting(1);
+function Str_to_remove_square($abc)
+{
+	$final_str = trim((str_replace(array('[',']',' '),array('','',''), $abc)));
+	return $final_str;
+}
+
+function Str_num($num1)
+{
+	$final_num = Str_to_remove_square(substr($num1, 0,strpos($num1, '<br>')));
+	return $final_num;
+}
+
+function Str_den($den1)
+{
+	$final_den = Str_to_remove_square(str_replace('<br>', '',strstr($den1, '<br>')));
+	return $final_den;
+}
+
+$v1 = "3,4,3";
+$v2 = "1,3,3";
+
+
+$param1 = "C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe test2.py convertMixedToImproperFraction $v1";
+$param2 = "C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe test2.py convertMixedToImproperFraction $v2";
+
+$result1 = Str_to_remove_square(shell_exec($param1));
+echo "convert mixed to improper fraction (v1): ".$result1."<br>";
+$result2 = Str_to_remove_square(shell_exec($param2));
+echo "convert mixed to improper fraction (v2): ".$result2."<br>";
+$param3 = "C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe test2.py computeListOfPrimeFactors $result1";
+$param4 = "C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe test2.py computeListOfPrimeFactors $result2";
+$result3 = shell_exec($param3);
+$result3_num = Str_num($result3);
+$result3_deno = Str_den($result3);
+$result4 = shell_exec($param4);
+$result4_num = Str_num($result4);
+$result4_deno = Str_den($result4);
+$conNUm = $result3_num.",".$result4_num;
+$conDen = $result3_deno.",".$result4_deno;
+echo "Prime Factor for numerator Variable 1: ".$result3_num."<br>";
+echo "Prime Factor for denominator Variable 1: ".$result3_deno."<br>";
+echo "Prime Factor for numerator Variable 2: ".$result4_num."<br>";
+echo "Prime Factor for denominator Variable 2: ".$result4_deno."<br>";
+echo "Concatenation of numerators (v1 & v2) = ".$conNUm."<br>";
+echo "Concatenation of numerators (v1 & v2) = ".$conDen."<br>";
+
+$param5 = "C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe test2.py cancelCommonFactors $conNUm $conDen";
+$result5 = shell_exec($param5);
+$result5_num = Str_num($result5);
+$result5_deno = Str_den($result5);
+echo "Cancel common factors of numerators (v1 & v2) =".$result5_num."<br>"; 
+echo "Cancel common factors of denominator (v1 & v2) =".$result5_deno."<br>";  
+
+$param6 = "C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe test2.py multiplyFactors $result5_num";
+$result6 = Str_to_remove_square(shell_exec($param6));
+echo "Multiply factors of numerators (v1 & v2) = ".$result6."<br>";
+
+
+
+$param7 = "C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe test2.py multiplyFactors $result5_deno";
+$result7 = Str_to_remove_square(shell_exec($param7));
+
+
+echo "Multiply factors of denominator (v1 & v2) = ".$result7."<br>"; 
+
+//var_dump($result7);
+
+$param8 = "C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe test2.py convertImproperToMixedFraction $result6 $result7";
+
+$result8 = Str_to_remove_square(shell_exec($param8));
+
+echo $result8;
+
+
+exit();
+
+
+
+
+
+
+
+?>
--- test2.py
+++ test2.py
@@ -1,5 +1,5 @@
 #!C:/Users/punit.RIVERBRIDGE/AppData/Local/Programs/Python/Python37-32/python.exe
-print("Content-type:text/html\r\n\r\n")
+# print("Content-type:text/html\r\n\r\n")
 
 primeNumbers = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
 
@@ -90,36 +90,98 @@ def multiplyFactors(cpf):
     
     return product
 
+if __name__ == '__main__':
+   import sys
+   x1 = sys.argv[1]
+   y1 = sys.argv[2]
 
-v1 = [1,4,3] #num den whole
-v2 = [4,9,4] #num den whole
-tv1 = convertMixedToImproperFraction(v1)
-tv2 = convertMixedToImproperFraction(v2)
-tv1NUMfactors = computeListOfPrimeFactors(tv1[0])
-tv1DEMfactors = computeListOfPrimeFactors(tv1[1])
-tv2NUMfactors = computeListOfPrimeFactors(tv2[0])
-tv2DEMfactors = computeListOfPrimeFactors(tv2[1])
+   # print(x1)
+     
+    # v1 = [3,4,3] #num den whole
 
-print("v1 = ",v1,"<br>")
-print("v2 = ",v2,"<br>")
-# print("%s",finaldata)
-print("Convert mixed to Improper for Variable 1 = ",tv1[0:2],"<br>")
-print("Convert mixed to Improper for Variable 2 = ",tv2[0:2],"<br>")
-print("Prime Factor for numerator for Variable 1 = ",tv1NUMfactors,"<br>")
-print("Prime Factor for denominator for Variable 1 = ",tv1DEMfactors,"<br>")
-print("Prime Factor for numerator for Variable 2 = ",tv2NUMfactors,"<br>")
-print("Prime Factor for denominator for Variable 2 = ",tv2DEMfactors,"<br>")
-conNUMfactors = tv1NUMfactors + tv2NUMfactors
-conDEMfactors = tv1DEMfactors + tv2DEMfactors
-print("Concatenation of numerators (v1 & v2) = ",conNUMfactors,"<br>")
-print("Concatenation of denominator (v1 & v2) = ",conDEMfactors,"<br>")
-finalNUMfactors, finalDEMfactors = cancelCommonFactors(conNUMfactors, conDEMfactors)
-print("Cancel common factors of numerators (v1 & v2) = ",finalNUMfactors,"<br>")
-print("Cancel common factors of denominator (v1 & v2) = ",finalDEMfactors,"<br>")
-res = [0,1,1]
-res[0] =  multiplyFactors( finalNUMfactors)
-res[1] =  multiplyFactors( finalDEMfactors)
-print("Multiply factors of numerators (v1 & v2) = ",finalNUM,"<br>")
-print("Multiply factors of denominator (v1 & v2) = ",finalDEM,"<br>")
-result = convertImproperToMixedFraction(res)
-print("Final result = ", result)
+
+if x1 == "convertMixedToImproperFraction":
+  my_list = y1.split(",") #convert string to array
+  my_list = list(map(int, my_list)) #Convert all strings in a list to int
+  tv1 = convertMixedToImproperFraction(my_list)
+  print(tv1[0:2]) 
+
+if x1 == "computeListOfPrimeFactors":
+  my_list = y1.split(",") #convert string to array
+  my_list = list(map(int, my_list)) #Convert all strings in a list to int
+  for i in range(len(my_list)):
+     computeval = computeListOfPrimeFactors(my_list[i])
+     print(computeval,"<br>");
+
+if x1 == "cancelCommonFactors":
+  z1 = sys.argv[3]
+  my_list = y1.split(",") #convert string to array
+  my_list = list(map(int, my_list)) #Convert all strings in a list to int
+  my_list2 = z1.split(",") #convert string to array
+  my_list2 = list(map(int, my_list2)) #Convert all strings in a list to int
+  finalNUMfactors,finalDEMfactors = cancelCommonFactors(my_list,my_list2)
+  print(finalNUMfactors,"<br>",finalDEMfactors)
+
+if x1 == "multiplyFactors":
+  my_list = y1.split(",") #convert string to array
+  my_list = list(map(int, my_list)) #Convert all strings in a list to int
+  str_mul_fact = multiplyFactors(my_list)
+  print(str_mul_fact)
+
+if x1 == "convertImproperToMixedFraction":
+  z1 = sys.argv[3]
+  my_list = int(y1) #Convert all strings in a list to int
+  my_list2 = int(z1) #Convert all strings in a list to int
+  res = [0,1,1]
+  res[0] = my_list
+  res[1] = my_list2
+  # print(res)
+  result = convertImproperToMixedFraction(res)
+  print(result)
+
+  # my_list = y1.split(",") #convert string to array
+  # my_list = list(map(int, my_list)) #Convert all strings in a list to int
+  # my_list2 = z1.split(",") #convert string to array
+  # my_list2 = list(map(int, my_list2)) #Convert all strings in a list to int
+  # my_list3 = mylist + my_list2
+  # print(my_list3)
+  # temp = convertImproperToMixedFraction(my_list,my_list2)
+  # str_mul_fact = multiplyFactors(my_list)
+  # print(str_mul_fact)
+  
+
+  
+
+# v1 = [3,4,3] #num den whole
+# v2 = [1,3,3] #num den whole
+# tv1 = convertMixedToImproperFraction(v1)
+# print(tv1)
+# tv2 = convertMixedToImproperFraction(v2)
+# tv1NUMfactors = computeListOfPrimeFactors(tv1[0])
+# tv1DEMfactors = computeListOfPrimeFactors(tv1[1])
+# tv2NUMfactors = computeListOfPrimeFactors(tv2[0])
+# tv2DEMfactors = computeListOfPrimeFactors(tv2[1])
+
+# print("v1 = ",v1,"<br>")
+# print("v2 = ",v2,"<br>")
+# # print("%s",finaldata)
+# print("Convert mixed to Improper for Variable 1 = ",tv1[0:2],"<br>")
+# print("Convert mixed to Improper for Variable 2 = ",tv2[0:2],"<br>")
+# print("Prime Factor for numerator for Variable 1 = ",tv1NUMfactors,"<br>")
+# print("Prime Factor for denominator for Variable 1 = ",tv1DEMfactors,"<br>")
+# print("Prime Factor for numerator for Variable 2 = ",tv2NUMfactors,"<br>")
+# print("Prime Factor for denominator for Variable 2 = ",tv2DEMfactors,"<br>")
+# conNUMfactors = tv1NUMfactors + tv2NUMfactors
+# conDEMfactors = tv1DEMfactors + tv2DEMfactors
+# print("Concatenation of numerators (v1 & v2) = ",conNUMfactors,"<br>")
+# print("Concatenation of denominator (v1 & v2) = ",conDEMfactors,"<br>")
+# finalNUMfactors, finalDEMfactors = cancelCommonFactors(conNUMfactors, conDEMfactors)
+# print("Cancel common factors of numerators (v1 & v2) = ",finalNUMfactors,"<br>")
+# print("Cancel common factors of denominator (v1 & v2) = ",finalDEMfactors,"<br>")
+# res = [0,1,1]
+# res[0] =  multiplyFactors( finalNUMfactors)
+# res[1] =  multiplyFactors( finalDEMfactors)
+# print("Multiply factors of numerators (v1 & v2) = ",res[0],"<br>")
+# print("Multiply factors of denominator (v1 & v2) = ",res[1],"<br>")
+# result = convertImproperToMixedFraction(res)
+# print("Final result = ", result)
